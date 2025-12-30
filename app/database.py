9d8fe7719cb297c567dd56ec5dc2de12b99acfa5
collins987 <vincentcollins690@gmail.com>
2025-12-30 19:08:59 +0300

    Initial commit

--- app/database.py
+++ app/database.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://sentineliq:sentineliq@postgres:5432/sentineliq_db"
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://sentineliq:sentineliq@postgres:5432/sentineliq")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
